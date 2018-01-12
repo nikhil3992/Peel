773e5fdb5bc25b92d3d956fa59db69ee8aa1c050
Nikhil Jonnalagadda <nikhil3992@gmail.com>
2018-01-12 15:34:08 -0500

    Updated models

--- src/com/peel/models/Content.java
+++ src/com/peel/models/Content.java
@@ -4,34 +4,29 @@ import java.util.List;
 
 public class Content {
 	
-	private String contentName;
-	private List<String> preRollNamesList;
-	private List<Video> contentVideoList;
+	private final String contentName;
+	private final List<String> preRollNamesList;
+	private final List<Video> contentVideoList;
 	
+	public Content(String contentName, List<String> preRollNamesList, List<Video> contentVideoList) {
+		
+		this.contentName = contentName;
+		this.preRollNamesList = preRollNamesList;
+		this.contentVideoList = contentVideoList;
+	}
+
 	public String getContentName() {
 		return contentName;
-	}
-	
-	public void setContentName(String contentName) {
-		this.contentName = contentName;
 	}
 	
 	public List<String> getPreRollNamesList() {
 		return preRollNamesList;
 	}
 
-	public void setPreRollNamesList(List<String> preRollNamesList) {
-		this.preRollNamesList = preRollNamesList;
-	}
-
 	public List<Video> getContentVideoList() {
 		return contentVideoList;
 	}
 	
-	public void setContentVideoList(List<Video> contentVideoList) {
-		this.contentVideoList = contentVideoList;
-	}
-
 	@Override
 	public String toString() {
 		return "Content [contentName=" + contentName + ", preRollNamesList=" + preRollNamesList + ", contentVideoList="
