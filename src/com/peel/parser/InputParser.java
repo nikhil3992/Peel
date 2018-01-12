a324926ec31a2ab2e13d0980c79ecc2646a6aa63
Nikhil Jonnalagadda <nikhil3992@gmail.com>
2018-01-12 00:35:31 -0500

    Corrected the indentation

--- src/com/peel/parser/InputParser.java
+++ src/com/peel/parser/InputParser.java
@@ -20,10 +20,10 @@ import com.peel.models.PreRoll;
 import com.peel.models.Video;
 
 
+// This class mainly contains methods to parse the JSON file and convert the data into models
 
 public class InputParser {
 
-	
 	/**
 	 * This method will take the filePath as argument and return
 	 * a reference to the root JSON object
@@ -36,9 +36,7 @@ public class InputParser {
 		JSONObject rootObject = null;
 		
 		try {
-			
 			rootObject = (JSONObject) new JSONParser().parse(new FileReader(filePath));
-		
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		
@@ -64,7 +62,6 @@ public class InputParser {
 	public Map<String,Content> getContentMap(List<Content> contentList) {
 		
 		Map<String,Content> contentMap = new HashMap<>();
-		
 		if(contentList.size() > 0) {
 			for(Content content : contentList) {
 				contentMap.put(content.getContentName(), content);
@@ -83,8 +80,7 @@ public class InputParser {
 	 */
 	public Map<String,PreRoll> getPreRollMap(List<PreRoll> preRollList) {
 		
-		Map<String,PreRoll> preRollMap = new HashMap<>();
-		
+		Map<String,PreRoll> preRollMap = new HashMap<>();		
 		if(preRollList.size() > 0) {
 			for(PreRoll preRoll : preRollList) {
 				preRollMap.put(preRoll.getName(), preRoll);
@@ -112,24 +108,24 @@ public class InputParser {
 		Iterator<JSONObject> contentIterator = contentArray.iterator();
 		
 		// iterate through each content object and add to contentList
-        while(contentIterator.hasNext()) {
+        	while(contentIterator.hasNext()) {
         	
-        	JSONObject contentJSONObject = contentIterator.next();    	
-        	Content content = new Content();
-        	
-        	// Get the Name of the content first
-        	String contentName = (String) contentJSONObject.get("name");
-        	content.setContentName(contentName);
-        	
-        	// Get preRoll names from preRollArray
-        	List<String> preRollNamesList = getPreRollNames(contentJSONObject);
-        	content.setPreRollNamesList(preRollNamesList);
-        	
-        	// Get videos from JSON Object
-        	List<Video> videosList = getVideosFromJSON(contentJSONObject);
-        	content.setContentVideoList(videosList);
-        	contentList.add(content);
-        }
+			JSONObject contentJSONObject = contentIterator.next();    	
+			Content content = new Content();
+
+			// Get the Name of the content first
+			String contentName = (String) contentJSONObject.get("name");
+			content.setContentName(contentName);
+
+			// Get preRoll names from preRollArray
+			List<String> preRollNamesList = getPreRollNames(contentJSONObject);
+			content.setPreRollNamesList(preRollNamesList);
+
+			// Get videos from JSON Object
+			List<Video> videosList = getVideosFromJSON(contentJSONObject);
+			content.setContentVideoList(videosList);
+			contentList.add(content);
+        	}
 		return contentList;
 	}
 	
@@ -146,23 +142,23 @@ public class InputParser {
 		List<String> preRollNamesList = new ArrayList<>();
 		
 		// Get the array of preRoll Names	
-    	JSONArray preRollNamesArray = (JSONArray) contentJSONObject.get("preroll");
+    		JSONArray preRollNamesArray = (JSONArray) contentJSONObject.get("preroll");
     	
-    	// Return empty list if array is null
-    	if(preRollNamesArray == null) return preRollNamesList;
+    		// Return empty list if array is null
+    		if(preRollNamesArray == null) return preRollNamesList;
     	
-    	Iterator<JSONObject> preRollNamesIterator = preRollNamesArray.iterator();
+    		Iterator<JSONObject> preRollNamesIterator = preRollNamesArray.iterator();
     	
-    	// Iterate through all the names and add to the list
-    	while(preRollNamesIterator.hasNext()) {
+    		// Iterate through all the names and add to the list
+    		while(preRollNamesIterator.hasNext()) {
     		
-    		JSONObject nameJSONObject = (JSONObject) preRollNamesIterator.next();
-    		String preRollName = (String) nameJSONObject.get("name");
-    		preRollNamesList.add(preRollName);
+			JSONObject nameJSONObject = (JSONObject) preRollNamesIterator.next();
+			String preRollName = (String) nameJSONObject.get("name");
+			preRollNamesList.add(preRollName);
     		
-    	}
+    		}
     	
-    	return preRollNamesList;
+    		return preRollNamesList;
 	}
 	
 	/**
@@ -180,27 +176,27 @@ public class InputParser {
 		// Return empty list if array is null
 		if(rootPreRollArray == null) return preRollList;
         
-        Iterator<JSONObject> preRollIterator = rootPreRollArray.iterator();
+        	Iterator<JSONObject> preRollIterator = rootPreRollArray.iterator();
         
-        // Iterate through preRolls objects and add to preRollList
-        while(preRollIterator.hasNext()) {
+        	// Iterate through preRolls objects and add to preRollList
+        	while(preRollIterator.hasNext()) {
         	
-        	JSONObject preRollJSONObject =  preRollIterator.next();
-        
-        	PreRoll preRoll = new PreRoll();
-        	
-        	// Get preRoll name from JSON object
-        	String preRollName = (String) preRollJSONObject.get("name");
-        	preRoll.setName(preRollName);
-        	
-        	// Get videos from JSON Object
-        	List<Video> videosList = getVideosFromJSON(preRollJSONObject);
-        	preRoll.setPreRollVideoList(videosList);
-        	
-        	// Add the preRoll to preRoll List
-        	preRollList.add(preRoll);	
-        }
-        return preRollList;
+			JSONObject preRollJSONObject =  preRollIterator.next();
+
+			PreRoll preRoll = new PreRoll();
+
+			// Get preRoll name from JSON object
+			String preRollName = (String) preRollJSONObject.get("name");
+			preRoll.setName(preRollName);
+
+			// Get videos from JSON Object
+			List<Video> videosList = getVideosFromJSON(preRollJSONObject);
+			preRoll.setPreRollVideoList(videosList);
+
+			// Add the preRoll to preRoll List
+			preRollList.add(preRoll);	
+        	}
+        	return preRollList;
 	}
 	
 	
@@ -216,27 +212,27 @@ public class InputParser {
 		JSONArray videosArray = (JSONArray) contentJSONObject.get("videos");
     	
 		// Iterate through all the videos 
-    	Iterator<JSONObject> videosIterator = videosArray.iterator();
+    		Iterator<JSONObject> videosIterator = videosArray.iterator();
     	
-    	List<Video> videosList = new ArrayList<>();
+    		List<Video> videosList = new ArrayList<>();
     	
-    	while(videosIterator.hasNext()) {
+    		while(videosIterator.hasNext()) {
     		
-    		Video video = new Video();
-    		
-    		JSONObject videoJSONObject = videosIterator.next();
-    		
-    		String name = (String) videoJSONObject.get("name");
-    		video.setVideoName(name);
-    		
-    		JSONObject attributeJSONObject = (JSONObject) videoJSONObject.get("attributes");
-    		Attribute attribute = getAttributeFromJSON(attributeJSONObject);
-    		video.setVideoAttribute(attribute);
-    		
-    		videosList.add(video);
-    	}
+			Video video = new Video();
+
+			JSONObject videoJSONObject = videosIterator.next();
+
+			String name = (String) videoJSONObject.get("name");
+			video.setVideoName(name);
+
+			JSONObject attributeJSONObject = (JSONObject) videoJSONObject.get("attributes");
+			Attribute attribute = getAttributeFromJSON(attributeJSONObject);
+			video.setVideoAttribute(attribute);
+
+			videosList.add(video);
+    		}
     	
-    	return videosList;
+    		return videosList;
 	}
 	
 	
